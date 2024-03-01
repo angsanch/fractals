bfd094eb57d4976dfa0d8d440cc3fd6c6c0bd43f
Sofía Martín Pajares <sofia.martin-pajares@epitech.eu>
2024-03-01 02:40:04 +0100

    FEAT: header file and gitignore

---
+++ include/my.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** rostring stumper05
+** File description:
+** headerfile
+*/
+
+#ifndef STUMPER_01_
+    #define STUMPER_01_
+
+    #include <sys/stat.h>
+    #include <fcntl.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <stdio.h>
+    #include <errno.h>
+    #include <stddef.h>
+
+void my_putchar(char c);
+int my_putstr(char const *str);
+int my_put_nbr(int nb);
+int my_str_len(char const *str);
+#endif
