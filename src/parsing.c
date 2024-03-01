4218344b2a0b89b0fa644dd6ccda592a02620196
Sofía Martín Pajares <sofia.martin-pajares@epitech.eu>
2024-03-01 03:45:59 +0100

    FEAT: parsing parameters

--- src/parsing.c
+++ src/parsing.c
@@ -5,21 +5,44 @@
 ** parsing the patters
 */
 
-int init_pattern(pattern *p, char *raw)
+#include "../include/fractals.h"
+#include "../include/my.h"
+
+static int first_len(char *p)
+{
+    int i = 0;
+
+    while (p[i] != '@'){
+	i++;
+    }
+    return (i);
+}
+
+static pat_lines(char *p)
+{
+    
+}
+
+static int init_pattern(pattern *p, char *raw)
 {
     int l = 0;
     int h = 0;
     int i = 0;
 
     for (int i = 0; p[i] != '\0'; i++){
-	if(p[i] != '#' || p[i] != '@' || p[i] != '.'){
+	if (p[i] != '#' || p[i] != '@' || p[i] != '.'){
 	    my_dputstr(2, "Error, invalid characters in pattern.\n");
+	    return (-1);
+	}
+	if (p[i] == '@' && p[i + 1] == '@'){
+	    my_dputstr(2, "Error, invalid characters in patter (two '@'s back together).\n");
 	    return (-1);
 	}
 	if (p[i] == '@')
 	    l++;
     }
     p->height = l + 1;
+    p->width = first_len(p);
     return (0);
 }
 
